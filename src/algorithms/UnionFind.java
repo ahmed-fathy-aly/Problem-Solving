5459f3da765d1f93322f74609b0707bee1ad67d6
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2017-04-23 14:29:13 +0200

    [DATA STRUCTURE] added the union find data structure
    unions disjoint sets

---
+++ src/algorithms/UnionFind.java
@@ -0,0 +1,58 @@
+package algorithms;
+
+import java.util.Arrays;
+
+/**
+ * Disjoint set union find with path compression and union by rank
+ * https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+ *
+ *
+ */
+public class UnionFind {
+
+    int n;
+    int[] parent, size;
+
+    public UnionFind(int n) {
+        this.n = n;
+        parent = new int[n];
+        Arrays.fill(parent, -1);
+        size = new int[n];
+        Arrays.fill(size, 1);
+    }
+
+    /**
+     * @return true if l and r belong to the same set
+     */
+    public boolean isJoined(int l, int r) {
+        return find(l) == find(r);
+    }
+
+    /**
+     * O(log(n))
+     * joins the sets of the two nodes l, r
+     * @return true if they are already joined
+     */
+    public boolean union(int l, int r) {
+        int root1 = find(l);
+        int root2 = find(r);
+        if (root1 == root2)
+            return true;
+        if (size[root2] > size[root1]){
+            int temp = root1;
+            root1 = root2;
+            root2 = temp;
+        }
+
+        size[root1] += size[root2];
+        parent[root2] = root1;
+        return false;
+    }
+
+    public int find(int node) {
+        if (parent[node] == -1)
+            return node;
+        return parent[node] = find(parent[node]);
+    }
+
+}
---
+++ test/algorithms/UnionFindTest.java
@@ -0,0 +1,103 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * Created by afathy on 4/23/17.
+ */
+public class UnionFindTest {
+
+    @Test
+    public void testSmall(){
+        UnionFind unionFind = new UnionFind(6);
+
+        assertFalse(unionFind.isJoined(1, 2));
+        unionFind.union(1, 2);
+        assertTrue(unionFind.isJoined(1, 2));
+
+        assertFalse(unionFind.isJoined(1, 3));
+        unionFind.union(2, 3);
+        assertTrue(unionFind.isJoined(1, 3));
+
+        assertFalse(unionFind.isJoined(1, 5));
+        unionFind.union(2, 5);
+        assertTrue(unionFind.isJoined(1, 5));
+    }
+
+    @Test
+    public void testRandom(){
+        int size = 10000;
+        UnionFind unionFind =new UnionFind(size);
+        DummyUnionFInd dummyUnionFInd = new DummyUnionFInd(size);
+
+        Random random = new Random();
+        int queries = 1000;
+        for (int i = 0; i < queries; i++){
+            int l = random.nextInt(size);
+            int r = random.nextInt(size);
+
+            boolean joined = unionFind.isJoined(l, r);
+            boolean joinedExpected = dummyUnionFInd.joined(l, r);
+            assertEquals(joinedExpected, joined);
+
+            unionFind.union(l, r);
+            dummyUnionFInd.union(l, r);
+        }
+    }
+
+    @Test
+    public void runningTimeTest(){
+        int size = 10000000;
+        UnionFind unionFind =new UnionFind(size);
+
+        Random random = new Random();
+        int queries = 10000000;
+        for (int i = 0; i < queries; i++){
+            int l = random.nextInt(size);
+            int r = random.nextInt(size);
+            unionFind.isJoined(l, r);
+            unionFind.union(l, r);
+        }
+    }
+    /**
+     * uses bfs for union find
+     * guaranteed to return the right answer so it's used for testing
+     */
+    class DummyUnionFInd{
+        boolean[][] g;
+        int n;
+
+        DummyUnionFInd(int n){
+                this.n = n;
+                g = new boolean[n][n];
+        }
+
+        public boolean joined(int l, int r){
+            boolean[] visited = new boolean[n];
+            visited[l] = true;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(l);
+            while (!q.isEmpty()){
+                int node = q.poll();
+                if (node == r)
+                    return true;
+                for (int next = 0; next < n; next++) {
+                    if (!visited[next] && g[node][next]){
+                        q.add(next);
+                        visited[next] = true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void union(int l, int r){
+            g[l][r] = true;
+            g[r][l] = true;
+        }
+    }
+}
